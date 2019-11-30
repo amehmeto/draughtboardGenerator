4dcf42815d8c75f99a3679c6b8e57c37d20c2e98
Arthur <42amehmeto@gmail.com>
2019-12-01 00:28:10 +0100

    :alembic: Adding four width parameter test

--- draughtboard_generator.test.js
+++ draughtboard_generator.test.js
@@ -36,7 +36,7 @@ it('should generate one line draughtboard', () => {
     expect(GENERATED_DRAUGHTBOARD).toStrictEqual(EXPECTED_DRAUGHTBOARD);
 });
 
-it('should generate two lines draughtboard', () => {
+it('should generate two lines draughtboard and default width', () => {
 	// Arrange
 	const EXPECTED_DRAUGHTBOARD =
 		[
@@ -48,4 +48,42 @@ it('should generate two lines draughtboard', () => {
 	const GENERATED_DRAUGHTBOARD = Draughtboard.generator(HEIGHT);
 	// Assert
 	expect(GENERATED_DRAUGHTBOARD).toStrictEqual(EXPECTED_DRAUGHTBOARD);
+});
+
+it('should generate seven lines draughtboard and default width', () => {
+	// Arrange
+	const EXPECTED_DRAUGHTBOARD =
+		[
+			['black','white','black','white','black','white'],
+			['white','black','white','black','white','black'],
+			['black','white','black','white','black','white'],
+			['white','black','white','black','white','black'],
+			['black','white','black','white','black','white'],
+			['white','black','white','black','white','black'],
+			['black','white','black','white','black','white'],
+		];
+	const HEIGHT = 7;
+	// Act
+	const GENERATED_DRAUGHTBOARD = Draughtboard.generator(HEIGHT);
+	// Assert
+	expect(GENERATED_DRAUGHTBOARD).toStrictEqual(EXPECTED_DRAUGHTBOARD);
+});
+
+it('should generate four cell width and default lines', () => {
+	// Arrange
+	const EXPECTED_DRAUGHTBOARD =
+		[
+			['black','white','black','white'],
+			['white','black','white','black'],
+			['black','white','black','white'],
+			['white','black','white','black'],
+			['black','white','black','white'],
+			['white','black','white','black'],
+			['black','white','black','white'],
+		];
+	const WIDTH = 4;
+	// Act
+	const GENERATED_DRAUGHTBOARD = Draughtboard.generator(WIDTH);
+	// Assert
+	expect(GENERATED_DRAUGHTBOARD).toStrictEqual(EXPECTED_DRAUGHTBOARD);dw
 });
